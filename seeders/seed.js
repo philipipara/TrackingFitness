05b0764e1dbad61a22b4cf4df22e4137416bcb2f
philipipara <54330369+philipipara@users.noreply.github.com>
2020-03-14 14:22:02 -0700

    adding

---
+++ seeders/seed.js
@@ -0,0 +1,191 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+let workoutSeed = [
+  {
+    day: new Date().setDate(new Date().getDate()-10),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bicep Curl",
+        duration: 20,
+        weight: 100,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-9),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Lateral Pull",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-8),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Push Press",
+        duration: 25,
+        weight: 185,
+        reps: 8,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-7),
+    exercises: [
+      {
+        type: "cardio",
+        name: "Running",
+        duration: 25,
+        distance: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-6),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 20,
+        weight: 285,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-5),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-4),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Quad Press",
+        duration: 30,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-3),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-2),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Military Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-1),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench",
+        duration: 30,
+        distance: 2
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-0),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Pull Ups",
+        duration: 4,
+        distance: 0
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-0),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Lat Raise",
+        duration: 15,
+        distance: 0
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-0),
+    exercises: [
+      {
+        type: "Cardio",
+        name: "Bike",
+        duration: 30,
+        distance: 3
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-0),
+    exercises: [
+      {
+        type: "Cardio",
+        name: "Last Workout",
+        duration: 30,
+        distance: 5
+      }
+    ]
+  }
+];
+
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
