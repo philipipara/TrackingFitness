b61ad578254ec822623e6f3eca49d8b0bc8b0c2a
philipipara <54330369+philipipara@users.noreply.github.com>
2020-03-14 17:53:35 -0700

    database link

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/Fitness", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
